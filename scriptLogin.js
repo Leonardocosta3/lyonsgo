0cb52a01b3c55889c522f03f53d9682deb4b205b
Leonardo Cabral da Costa <138399471+Leonardocosta3@users.noreply.github.com>
2024-01-11 20:46:56 -0300

    update lg

--- scriptLogin.js
+++ scriptLogin.js
@@ -1,11 +1,9 @@
-/*Área de Login */
-/*Navegação entre divs adm */
 var btnEntrar = document.getElementById('login-btn-entrar');
 
 btnEntrar.addEventListener('click', function(){
-    var ctnLogin = document.getElementById('container-login');
-    var ctnPa = document.getElementById('container-pa');
-    var ctnRs = document.getElementById('container-rs');
+    var ctnLogin = document.getElementById('container-lo');
+    var ctnPa = document.getElementById('container-Pa');
+    var ctnRs = document.getElementById('container-Rs');
 
     ctnLogin.style.display = "none";
     ctnPa.style.display = "flex";

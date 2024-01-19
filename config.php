0c263e6403f4b5e784a7015db38733df4e07e506
Leonardo Cabral da Costa <138399471+Leonardocosta3@users.noreply.github.com>
2024-01-19 14:54:10 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'usuariosgo';
+    
+    $conexao = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+
+
+?>

b56aa2b4cbb0822f531805ae8268398e1cbfc279
Leonardo Cabral da Costa <138399471+Leonardocosta3@users.noreply.github.com>
2024-01-19 00:39:02 -0300

    update lg

--- scriptLogin.js
+++ scriptLogin.js
@@ -20,9 +20,6 @@ btnSemsenhalo.addEventListener('click', function () {
 });
 
 btnSalvar.addEventListener('click', function () {
-    containerLogin.classList.remove('hidden');
-    containerPa.classList.add('hidden');
-    containerRs.classList.add('hidden');
 
 });
 
@@ -75,28 +72,35 @@ class Clientes {
     constructor() {
         this.id = 1;
         this.arrayClientes = [];
-
     }
 
     entrar() {
-       let clientes = this.lerDados();
 
-       if(this.validaCampos(clientes)){
-           if(this.validaUser() == true){
+        let clientes = this.lerDados();
+        
+        if (this.validaCampos(clientes)) {
+            if (this.validaUser() === true) {
 
-            containerLogin.classList.add('hidden');
-            containerPa.classList.remove('hidden');
-            containerRs.classList.add('hidden');
+                containerLogin.classList.add('hidden');
+                containerPa.classList.remove('hidden');
+                containerRs.classList.add('hidden');
 
-           }else if(this.validaAdm() == true){
-            location.href = 'adm.html';
-           }
-       }
-
+            } else if (this.validaAdm() === true) {
+                location.href = 'adm.html';
+            }else{
+                alert('Senha Inválida!')
+            }
+        }
     }
     salvar() {
-
+        if(this.validaCampoSenha()){
+            this.novaSenha();
+        }
+        containerLogin.classList.remove('hidden');
+        containerPa.classList.add('hidden');
+        containerRs.classList.add('hidden');
     }
+
 
     validaCampos(clientes) {
         let msg = "";
@@ -127,40 +131,80 @@ class Clientes {
     }
 
     novaSenha() {
+        const nomeUsuario = document.getElementById("email").value;
+        const novaSenha = document.getElementById("newpassword").value;
+        const confirmaSenha = document.getElementById("newpassword2").value;
 
+        if (novaSenha !== confirmaSenha) {
+            alert("As senhas não coincidem. Por favor, confirme a senha corretamente.");
+            return;
+        }
+
+        // Encontrar o índice do usuário no array
+        const index = users.findIndex(loginUser => loginUser.loginUser === nomeUsuario);
+
+        // Verificar se o usuário foi encontrado
+        if (index !== -1) {
+            // Atualizar a senha
+            users[index].senhaUser = novaSenha;
+            alert("Senha atualizada com sucesso!");
+        } else {
+            alert("Usuário não encontrado!");
+        }
     }
-    
-    validaUser(){
+    validaCampoSenha(){
+        const novaSenha = document.getElementById("newpassword").value;
+        const confirmaSenha = document.getElementById("newpassword2").value;
+
+        let msg = "";
+
+        if (novaSenha == "") {
+            msg += "- Informe uma Nova Senha \n"
+        }
+        if (confirmaSenha == "") {
+            msg += "- Informe uma Senha Senha \n"
+        }
+        if (msg != "") {
+            alert(msg)
+            return false
+        }
+        return true;
+    }
+
+    validaUser() {
         var nomeUsuario = document.getElementById('email').value;
         var senhaUsuario = document.getElementById('password').value;
-        
+
         for (let i = 0; i < users.length; i++) {
             const usuarioAtual = users[i];
-    
-            if (usuarioAtual.loginUser === nomeUsuario &&       usuarioAtual.senhaUser === senhaUsuario) {
-                
-                return true; 
+
+            if (usuarioAtual.loginUser === nomeUsuario && usuarioAtual.senhaUser === senhaUsuario) {
+
+                return true;
             }
         }
         return false;
-        
+
     }
     validaAdm() {
         var nomeAdm = document.getElementById('email').value;
         var senhaAdm = document.getElementById('password').value;
 
-        
+
         for (let i = 0; i < adms.length; i++) {
             const admAtual = adms[i];
-    
+
             if (admAtual.loginAdm === nomeAdm && admAtual.senhaAdm === senhaAdm) {
-                
+
                 return true;
             }
         }
         return false;
     }
     semSenha() {
+
+    }
+    comsenha(){
         
     }
 }
